kmhigashioka/dotnet-core-boilerplate

Rename dbcontext

NetCoreWebApiPoC.Application.Tests/Todos/Queries/GetTodosQueryTests.cs
@@ -18,7 +18,7 @@ namespace NetCoreWebApiPoC.Application.Tests.Todos.Queries
         [Fact]
         public void Get_Todos()
         {
-            var mockContext = new Mock<ITodoContext>();
+            var mockContext = new Mock<INetCoreWebApiPoCContext>();
             var mockDbSetTodos = new List<Todo>
             {
                 new Todo
@@ -44,7 +44,7 @@ namespace NetCoreWebApiPoC.Application.Tests.Todos.Queries
             Assert.Equal(2, result.Result.Count);
         }
 
-        private static IMediator BuildMediator(ITodoContext context)
+        private static IMediator BuildMediator(INetCoreWebApiPoCContext context)
         {
             var services = new ServiceCollection();
 

NetCoreWebApiPoC.Application/Todos/Commands/NewTodo/NewTodoCommand.cs
@@ -15,9 +15,9 @@ namespace NetCoreWebApiPoC.Application.Todos.Commands.NewTodo
 
     public class NewTodoCommandHandler : IRequestHandler<NewTodoCommand, TodoDto>
     {
-        private readonly ITodoContext _context;
+        private readonly INetCoreWebApiPoCContext _context;
 
-        public NewTodoCommandHandler(ITodoContext context)
+        public NewTodoCommandHandler(INetCoreWebApiPoCContext context)
         {
             _context = context;
         }

NetCoreWebApiPoC.Application/Todos/Queries/GetTodos/GetTodosQuery.cs
@@ -16,9 +16,9 @@ namespace NetCoreWebApiPoC.Application.Todos.Queries.GetTodos
 
     public class GetTodosQueryHandler : IRequestHandler<GetTodosQuery, List<Todo>>
     {
-        private readonly ITodoContext _context;
+        private readonly INetCoreWebApiPoCContext _context;
 
-        public GetTodosQueryHandler(ITodoContext context)
+        public GetTodosQueryHandler(INetCoreWebApiPoCContext context)
         {
             _context = context;
         }

NetCoreWebApiPoC.WebUI/Program.cs
@@ -19,7 +19,7 @@ namespace NetCoreWebApiPoC.WebUI
                 var services = scope.ServiceProvider;
                 try
                 {
-                    var context = services.GetRequiredService<ITodoContext>();
+                    var context = services.GetRequiredService<INetCoreWebApiPoCContext>();
                     DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)

NetCoreWebApiPoC.WebUI/Startup.cs
@@ -27,11 +27,11 @@ namespace NetCoreWebApiPoC.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<NetCoreWebApiPoCContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddMediatR(typeof(NewTodoCommand).GetTypeInfo().Assembly);
             services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<TodoContext>()
+                .AddEntityFrameworkStores<NetCoreWebApiPoCContext>()
                 .AddDefaultTokenProviders();
             services.AddMvcCore()
                 .AddAuthorization()
@@ -55,7 +55,7 @@ namespace NetCoreWebApiPoC.WebUI
                     options.RequireHttpsMetadata = false;
                     options.ApiName = "api1";
                 });
-            services.AddScoped<ITodoContext>(provider => provider.GetService<TodoContext>());
+            services.AddScoped<INetCoreWebApiPoCContext>(provider => provider.GetService<NetCoreWebApiPoCContext>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

kmhigashioka/netcore-jump-start

NetCoreWebApiPoC.Application.Tests/Todos/Queries/GetTodosQueryTests.cs
@@ -18,7 +18,7 @@ namespace NetCoreWebApiPoC.Application.Tests.Todos.Queries
         [Fact]
         public void Get_Todos()
         {
-            var mockContext = new Mock<ITodoContext>();
+            var mockContext = new Mock<IAppContext>();
             var mockDbSetTodos = new List<Todo>
             {
                 new Todo
@@ -44,7 +44,7 @@ namespace NetCoreWebApiPoC.Application.Tests.Todos.Queries
             Assert.Equal(2, result.Result.Count);
         }
 
-        private static IMediator BuildMediator(ITodoContext context)
+        private static IMediator BuildMediator(IAppContext context)
         {
             var services = new ServiceCollection();
 

NetCoreWebApiPoC.Application/Todos/Commands/NewTodo/NewTodoCommand.cs
@@ -15,9 +15,9 @@ namespace NetCoreWebApiPoC.Application.Todos.Commands.NewTodo
 
     public class NewTodoCommandHandler : IRequestHandler<NewTodoCommand, TodoDto>
     {
-        private readonly ITodoContext _context;
+        private readonly IAppContext _context;
 
-        public NewTodoCommandHandler(ITodoContext context)
+        public NewTodoCommandHandler(IAppContext context)
         {
             _context = context;
         }

NetCoreWebApiPoC.Application/Todos/Queries/GetTodos/GetTodosQuery.cs
@@ -16,9 +16,9 @@ namespace NetCoreWebApiPoC.Application.Todos.Queries.GetTodos
 
     public class GetTodosQueryHandler : IRequestHandler<GetTodosQuery, List<Todo>>
     {
-        private readonly ITodoContext _context;
+        private readonly IAppContext _context;
 
-        public GetTodosQueryHandler(ITodoContext context)
+        public GetTodosQueryHandler(IAppContext context)
         {
             _context = context;
         }

NetCoreWebApiPoC.WebUI/Program.cs
@@ -19,7 +19,7 @@ namespace NetCoreWebApiPoC.WebUI
                 var services = scope.ServiceProvider;
                 try
                 {
-                    var context = services.GetRequiredService<ITodoContext>();
+                    var context = services.GetRequiredService<IAppContext>();
                     DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)

NetCoreWebApiPoC.WebUI/Startup.cs
@@ -27,11 +27,11 @@ namespace NetCoreWebApiPoC.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddMediatR(typeof(NewTodoCommand).GetTypeInfo().Assembly);
             services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<TodoContext>()
+                .AddEntityFrameworkStores<AppContext>()
                 .AddDefaultTokenProviders();
             services.AddMvcCore()
                 .AddAuthorization()
@@ -55,7 +55,7 @@ namespace NetCoreWebApiPoC.WebUI
                     options.RequireHttpsMetadata = false;
                     options.ApiName = "api1";
                 });
-            services.AddScoped<ITodoContext>(provider => provider.GetService<TodoContext>());
+            services.AddScoped<IAppContext>(provider => provider.GetService<AppContext>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.