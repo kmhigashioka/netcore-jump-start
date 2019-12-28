ddb0f2cc1dba7b750ac344cbab55d5c7ce68a608
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-12-28 09:05:28 +0800

    Migrate to netcore2.1 (#11)
    * Migrate to netcoreapp2.1
    
    * Migrate to 2.1

--- NetCoreWebApiPoC.WebUI/Program.cs
+++ NetCoreWebApiPoC.WebUI/Program.cs
@@ -12,7 +12,7 @@ namespace NetCoreWebApiPoC.WebUI
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
+            var host = CreateWebHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
             {
@@ -32,9 +32,8 @@ namespace NetCoreWebApiPoC.WebUI
             host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
     }
 }
