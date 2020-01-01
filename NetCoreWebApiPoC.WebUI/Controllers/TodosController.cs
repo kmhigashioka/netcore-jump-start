6ed70c043e8a17d7202ca79ebe7ddd540a075a6b
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-01-01 13:42:12 +0800

    Configure swagger (#15)
    * Configure swagger
    
    * Update swagger
    
    * Add api documentation

--- NetCoreWebApiPoC.WebUI/Controllers/TodosController.cs
+++ NetCoreWebApiPoC.WebUI/Controllers/TodosController.cs
@@ -1,9 +1,12 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreWebApiPoC.Application.Dto;
 using NetCoreWebApiPoC.Application.Todos.Commands.NewTodo;
 using NetCoreWebApiPoC.Application.Todos.Queries.GetTodos;
+using NetCoreWebApiPoC.Domain.Entities;
+using System.Collections.Generic;
 
 namespace NetCoreWebApiPoC.WebUI.Controllers
 {
@@ -19,21 +22,34 @@ namespace NetCoreWebApiPoC.WebUI.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Get Todo items.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesResponseType(typeof(List<Todo>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult GetTodos()
         {
             var result = _mediator.Send(new GetTodosQuery());
             return Ok(result.Result);
         }
 
+        /// <summary>
+        /// Create Todo item.
+        /// </summary>
+        /// <param name="todo"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(typeof(Todo), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult Create([FromBody] TodoDto todo)
         {
             var result = _mediator.Send(new NewTodoCommand {
                 Todo = todo
             });
 
-            return Created(string.Empty, result.Result);
+            return CreatedAtRoute("GetTodos", null, result);
         }
     }
 }
--- NetCoreWebApiPoC.WebUI/Startup.cs
+++ NetCoreWebApiPoC.WebUI/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using IdentityServer4.AccessTokenValidation;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -11,8 +12,8 @@ using Microsoft.Extensions.Hosting;
 using NetCoreWebApiPoC.Application.Interfaces;
 using NetCoreWebApiPoC.Application.Todos.Commands.NewTodo;
 using NetCoreWebApiPoC.Domain.Entities;
-using NetCoreWebApiPoC.Persistence;
 using NetCoreWebApiPoC.WebUI.Configuration;
+using AppContext = NetCoreWebApiPoC.Persistence.AppContext;
 
 namespace NetCoreWebApiPoC.WebUI
 {
@@ -57,6 +58,9 @@ namespace NetCoreWebApiPoC.WebUI
                     options.ApiName = "api1";
                 });
             services.AddScoped<IAppContext>(provider => provider.GetService<AppContext>());
+            services.AddSwaggerGen(provider => 
+                provider.SwaggerDoc("latest", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Core API", Description = "Swagger Core API" }
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +75,12 @@ namespace NetCoreWebApiPoC.WebUI
             app.UseIdentityServer();
             app.UseMiddleware(typeof(ErrorHandlingMiddleware));
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(p =>
+            {
+                p.SwaggerEndpoint("/swagger/latest/swagger.json", "default");
+                p.RoutePrefix = string.Empty;
+            });
             app.UseFileServer();
         }
     }
